8b05f524824bf2ac2f2d4ff8dfcc87c6217453f6
SKStans9 <SKStans9@gmail.com>
2018-08-31 10:20:06 +0700

    Test

---
+++ src/OOP.java
@@ -0,0 +1,13 @@
+
+public class OOP {
+	private String description;
+	public OOP(String description) {
+		this.description = description;
+	}
+	public String getdes() {
+		return description;
+	}
+	public void setdes(String description) {
+		this.description = description;
+	}
+}
---
+++ src/OOPtest.java
@@ -0,0 +1,9 @@
+
+public class OOPtest {
+	public static void main(String[]args) {
+		OOP OOPONE = new OOP("Jam");
+		System.out.printf(" %s description ",OOPONE.getdes());
+		OOPONE.setdes("Sun");
+		System.out.printf(" %s description ",OOPONE.getdes());
+	}
+}
